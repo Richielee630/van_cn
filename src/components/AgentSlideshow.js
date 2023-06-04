7780aac9f6ab9982d74da1d6dcf5ee43aafee298
Richielee630 <richielee630@icloud.com>
2023-06-04 12:41:00 -0700

    add two more agents

--- src/components/AgentSlideshow.js
+++ src/components/AgentSlideshow.js
@@ -28,6 +28,18 @@ const AgentSlideshow = () => {
       description: 'Super, known by his codename, is a formidable attacker within the ranks of VAN_CN. With exceptional physical strength and agility, Super possesses unparalleled combat skills. His training in martial arts combined with his natural athleticism allows him to overpower adversaries with ease. Super\'s raw power, combined with his strategic thinking, makes him a force to be reckoned with on the field. Despite his impressive physical capabilities, Super is also known for his relentless work ethic and dedication to honing his skills. His unwavering determination to protect the innocent and uphold justice makes him a vital asset in VAN_CN\'s fight against crime and terrorism.',
       audioUrl: '/assets/audio/agent3-audio.wav', 
     },
+    {
+      name:'Sebie.Van_CN',
+      photo: '/assets/images/agent4.jpg',
+      description: 'Sebie is a head officer for VAN_CN\'s GERMAN department. With expertise in assassination, he is a highly skilled and meticulous agent. Sebie operates in a covert and discreet manner, always maintaining an air of mystery. His ability to blend into any environment and execute precise missions without detection makes him an invaluable asset to VAN_CN. Sebie\'s calm and composed demeanor allows him to make quick decisions under pressure, ensuring the success of critical operations. His colleagues respect his dedication, professionalism, and unwavering commitment to protecting the agency\'s interests.',
+      audioUrl: '/assets/audio/agent4-audio.wav',
+    },
+    {
+      name:'Zy3.Van_CN',
+      photo: '/assets/images/agent5.jpg',
+      description:'Zy3 is a key member of VAN_CN\'s quick response unit. Working closely with Clint, he provides tactical support during missions. Zy3\'s expertise lies in strategizing and executing rapid responses in dynamic situations. His quick thinking and adaptability make him an asset when faced with unpredictable challenges. Despite the high-pressure nature of his role, Zy3 maintains an easygoing and humorous disposition. His ability to lighten the mood and boost team morale during intense missions is greatly appreciated by his colleagues. Zy3\'s dedication, teamwork, and tactical prowess make him an invaluable member of the VAN_CN agency.',
+      audioUrl: '/assets/audio/agent5-audio.wav',
+    }
   ];
 
   const [activeIndex, setActiveIndex] = useState(0);
@@ -45,7 +57,7 @@ const AgentSlideshow = () => {
   };
 
   const settings = {
-    dots: true,
+    // dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
